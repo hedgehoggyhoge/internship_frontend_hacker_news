4894839400c4064cad5145ab2ad03d946c8802d7
a.shirobokova <anastasiya@superdao.co>
2022-11-15 16:51:19 +0300

    reducer upgrade

--- src/redux/reducer/loaderReducer.js
+++ src/redux/reducer/loaderReducer.js
@@ -1,27 +1,18 @@
-const initialState = {
-    loading: false,
-}
+const firstState = {loading: false,}
+const loader_true = 'loader_true'
+const loader_false = 'loader_false'
 
-const LOADER_DISPLAY_ON = 'LOADER_DISPLAY_ON'
-const LOADER_DISPLAY_OFF = 'LOADER_DISPLAY_OFF'
-
-export const loaderReducer = (state = initialState, action) => {
+export const loaderReducer = (state = firstState, action) => {
     switch (action.type){
+        case loader_true:
+            return {...state, loading: true}
 
-        case LOADER_DISPLAY_ON:
-            return {
-                ...state,
-                loading: true
-            }
-        case LOADER_DISPLAY_OFF:
-            return {
-                ...state,
-                loading: false
-            }
+        case loader_false:
+            return {...state, loading: false}
         default:
             return state
     }
 }
-export const loaderOnAction = () => ({type: LOADER_DISPLAY_ON})
-export const loaderOffAction = () => ({type: LOADER_DISPLAY_OFF})
 
+export const loaderOnAction = () => ({type: loader_true})
+export const loaderOffAction = () => ({type: loader_false})
